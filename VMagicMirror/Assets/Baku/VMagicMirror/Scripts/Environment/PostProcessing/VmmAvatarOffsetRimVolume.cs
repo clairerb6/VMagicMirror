7a51179c1cc613c963925f9e45dad666c5ca020d
Baku Dreameater <njslyr.trpg+harriet@gmail.com>
2026-05-04 00:01:56 +0900

    temp: use polar coordinate to check look

--- VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Environment/PostProcessing/VmmAvatarOffsetRimVolume.cs
+++ VMagicMirror/Assets/Baku/VMagicMirror/Scripts/Environment/PostProcessing/VmmAvatarOffsetRimVolume.cs
@@ -12,7 +12,10 @@ namespace Baku.VMagicMirror
     public sealed class VmmAvatarOffsetRimVolume : VolumeComponent
     {
         public BoolParameter enabled = new(false);
+        // NOTE: ほんとはoffsetを保持したほうがいいが、絵面のチェック用に一時的に極座標系を導入している
         public Vector2Parameter offset = new(Vector2.zero);
+        public ClampedFloatParameter offsetAngle = new(0f, 0f, 360f);
+        public ClampedFloatParameter offsetMilliMagnitude = new(0f, 0f, 10f);
         public ColorParameter rimColor = new(Color.white, false, false, true);
         public ClampedFloatParameter applyRate = new(1f, 0f, 1f);
     }
